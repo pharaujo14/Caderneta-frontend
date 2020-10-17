5fc12cb9dd99b6277835413b8ddd32572279c75c
Paulo Silva <pharaujo14@gmail.com>
2020-10-17 13:07:16 -0300

    Criadas funções validarSenha e showPassword", retirado campo foto, criado span com o icone de olho para mostrar a senha

--- js/validaForm.js
+++ js/validaForm.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
+  showPassword();
+  $('#validaSenha').css("display", "none") 
   $("#cpf").mask("999.999.999-99");
   $("#formCadastro").submit(function (event) {
     event.preventDefault();
-
-    enviar();
+    validarSenha(); 
   });
 });
+
+function validarSenha(){
+  senha = $("#senha").val();
+  confirmaSenha = $("#confirmasenha").val();
+  if (senha != confirmaSenha){ 
+    $('#validaSenha').css("display", "grid") 
+    senha == null;
+    confirmaSenha == null;
+    return false;
+  }
+  $('#validaSenha').css("display", "none") 
+  enviar();
+  return true;
+}
 
 function enviar() {
   var insert = {};
@@ -13,7 +28,6 @@ function enviar() {
   insert["sobrenome"] = $("#sobrenome").val();
   insert["email"] = $("#email").val();
   insert["cpf"] = $("#cpf").val().replace(/[^\d]+/g,'');
-  insert["foto"] = $("#foto").val();
   insert["senha"] = $("#senha").val();
 
   $("#enviar").prop("disabled", true);
@@ -29,3 +43,28 @@ function enviar() {
     sucess: alert("Cadastro realizado com sucesso")
   });
 }
+
+
+function showPassword(){
+  var senha = $('#senha');
+  var confirmaSenha = $('#confirmasenha');
+  var senhaOlho= $("#senhaOlho");
+  var confirmaSenhaOlho= $("#confirmaSenhaOlho");
+  
+    senhaOlho.mousedown(function() {
+      return senha.attr("type", "text");
+    });
+    
+    senhaOlho.mouseup(function() {
+      return senha.attr("type", "password");
+    });
+  
+    confirmaSenhaOlho.mousedown(function() {
+      return confirmaSenha.attr("type", "text");
+    });
+    
+    confirmaSenhaOlho.mouseup(function() {
+      return confirmaSenha.attr("type", "password");
+    });
+}
+
