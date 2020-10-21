f754cd7f5f4afe654b0820ae3b96e66fe7818911
Paulo Silva <pharaujo14@gmail.com>
2020-10-21 19:24:43 -0300

    Ajustes na página CadastroDeTurmas

--- js/validaCadastraTurmas.js
+++ js/validaCadastraTurmas.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
+    ajustaData();
     $("#formCadastroTurma").submit(function (event) {
         event.preventDefault();
         enviar();
     });
 });
 
+function ajustaData(){
+    $('[type="date"]').on("keydown", function() {
+        event.preventDefault();
+        return false;
+      });
+}
+
 function enviar() {
     var insert = {};
     insert["nome"] = $("#nomeTurma").val();
     insert["local"] = $("#local").val();
-    insert["horario"] = $("#horarioAula").val();
+    insert["horarioInicio"] = $("#horaInicio").val();
+    insert["horarioFim"] = $("#horaEncerramento").val();
     insert["dia"] = $("#diasAulas").val();
     insert["inicio"] = $("#inicioAulas").val().replace(/[^\d]+/g, '');
     insert["fim"] = $("#encerramentoAulas").val().replace(/[^\d]+/g, '');
