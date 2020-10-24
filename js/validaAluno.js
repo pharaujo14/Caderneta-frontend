2e8cf10a3005f1da8a265a08a542566acd6ac556
Lucas R. Souza <lucasrsouz4@gmail.com>
2020-10-23 21:27:52 -0300

    Alterações no cadastro inicial e cadastro de turmas

---
+++ js/validaAluno.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+  showPassword();
+  $('#validaSenha').css("display", "none") 
+  $("#cpf").mask("999.999.999-99");
+  $("#formCadastro").submit(function (event) {
+    event.preventDefault();
+    validarSenha(); 
+  });
+});
+
+function validarSenha(){
+  senha = $("#senha").val();
+  confirmaSenha = $("#confirmasenha").val();
+  if (senha != confirmaSenha){ 
+    $('#validaSenha').css("display", "grid") 
+    senha == null;
+    confirmaSenha == null;
+    return false;
+  }
+  $('#validaSenha').css("display", "none") 
+  enviar();
+  return true;
+}
+
+function enviar() {
+  var insert = {};
+  insert["nome"] = $("#nome").val();
+  insert["sobrenome"] = $("#sobrenome").val();
+  insert["email"] = $("#email").val();
+  insert["cpf"] = $("#cpf").val().replace(/[^\d]+/g,'');
+  insert["senha"] = $("#senha").val();
+
+  $("#enviar").prop("disabled", true);
+
+  $.ajax({
+    type: "POST",
+    contentType: "application/json",
+    url: "http://localhost:8080/alunos",
+    data: JSON.stringify(insert),
+    dataType: "json",
+    cache: false,
+    timeout: 600000,
+    sucess: alert("Cadastro realizado com sucesso")
+  });
+}
+
+
+function showPassword(){
+  var senha = $('#senha');
+  var confirmaSenha = $('#confirmasenha');
+  var senhaOlho= $("#senhaOlho");
+  var confirmaSenhaOlho= $("#confirmaSenhaOlho");
+  
+    senhaOlho.mousedown(function() {
+      return senha.attr("type", "text");
+    });
+    
+    senhaOlho.mouseup(function() {
+      return senha.attr("type", "password");
+    });
+  
+    confirmaSenhaOlho.mousedown(function() {
+      return confirmaSenha.attr("type", "text");
+    });
+    
+    confirmaSenhaOlho.mouseup(function() {
+      return confirmaSenha.attr("type", "password");
+    });
+}
+
